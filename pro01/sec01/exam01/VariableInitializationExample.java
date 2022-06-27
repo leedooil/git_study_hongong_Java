f8a35a3f25c971a3bbe278f88e4be843be08aa0c
leedooil <ldi316@naver.com>
2022-06-27 21:50:18 +0900

    20220627

---
+++ pro01/sec01/exam01/VariableInitializationExample.java
@@ -0,0 +1,12 @@
+package sec01.exam01;
+
+public class VariableInitializationExample {
+	public static void main(String[] args) {
+		
+		int value = 20;
+		int result = value + 30;
+		System.out.println(result); //println은 이미 만들어진 라이브러리
+		
+	}
+
+}
